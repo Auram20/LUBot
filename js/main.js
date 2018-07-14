5cca4f0a09ef75e848525639ee14386fe04a40c1
Auram20 <myriamanik97@gmail.com>
2018-07-14 18:45:25 +0200

    Animation triggered by scrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
 $(document).ready(function() {
    $('#nav').localScroll({duration:800});
 });
+
+
+
+// TRIGGERS THE APPEARANCE OF FIRST PART
+var controller1 = new ScrollMagic.Controller();
+
+var scene1 = new ScrollMagic.Scene({
+		triggerElement: '.firstpart'
+    })
+	scene1.setClassToggle('.totrigger1','zoomIn');
+    scene1.addTo(controller1); // assign the scene to the controller
