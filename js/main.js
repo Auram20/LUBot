2df364af57a27e5660ea5021bc86f6d651bed955
Auram20 <myriamanik97@gmail.com>
2018-07-14 20:00:24 +0200

    more and more animations

--- js/main.js
+++ js/main.js
@@ -28,3 +28,37 @@ var scene3 = new ScrollMagic.Scene({
     })
 	scene3.setClassToggle('.totrigger3','zoomIn');
     scene3.addTo(controller3); // assign the scene to the controller
+
+        // TRIGGERS THE APPEARANCE OF FOURTH PART
+var controller4 = new ScrollMagic.Controller();
+var scene4 = new ScrollMagic.Scene({
+		triggerElement: '.fourthpart'
+    })
+	scene4.setClassToggle('.totrigger4','fadeInRight');
+    scene4.addTo(controller4); // assign the scene to the controller
+
+var controller5 = new ScrollMagic.Controller();
+var scene5 = new ScrollMagic.Scene({
+		triggerElement: '.fourthpart'
+    })
+	scene5.setClassToggle('.totrigger5','fadeInLeft');
+    scene5.addTo(controller5); // assign the scene to the controller
+
+var controller6 = new ScrollMagic.Controller();
+var scene6 = new ScrollMagic.Scene({
+		triggerElement: '.fifthpart'
+    })
+	scene6.setClassToggle('.totrigger6','zoomIn');
+    scene6.addTo(controller6); // assign the scene to the controller
+
+
+var controller7 = new ScrollMagic.Controller();
+var scene7 = new ScrollMagic.Scene({
+		triggerElement: '.fifthpart'
+    })
+	scene7.setClassToggle('.totrigger7','zoomIn');
+    scene7.addTo(controller7); // assign the scene to the controller
+
+
+
+
