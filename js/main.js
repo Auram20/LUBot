1a7d522139c41c3f0bdc61cce3adf0aefc69d2ea
Auram20 <myriamanik97@gmail.com>
2018-07-14 18:58:29 +0200

    more animations trigerred

--- js/main.js
+++ js/main.js
@@ -7,9 +7,24 @@ $(document).ready(function() {
 
 // TRIGGERS THE APPEARANCE OF FIRST PART
 var controller1 = new ScrollMagic.Controller();
-
 var scene1 = new ScrollMagic.Scene({
 		triggerElement: '.firstpart'
     })
 	scene1.setClassToggle('.totrigger1','zoomIn');
     scene1.addTo(controller1); // assign the scene to the controller
+
+// TRIGGERS THE APPEARANCE OF SECOND PART
+var controller2 = new ScrollMagic.Controller();
+var scene2 = new ScrollMagic.Scene({
+		triggerElement: '.secondpart'
+    })
+	scene2.setClassToggle('.totrigger2','zoomIn');
+    scene2.addTo(controller2); // assign the scene to the controller
+
+    // TRIGGERS THE APPEARANCE OF THIRS PART
+var controller3 = new ScrollMagic.Controller();
+var scene3 = new ScrollMagic.Scene({
+		triggerElement: '.thirdpart'
+    })
+	scene3.setClassToggle('.totrigger3','zoomIn');
+    scene3.addTo(controller3); // assign the scene to the controller
